eee4d97e04f2c66a8de823caec20121cc9b02334
choppen5 <choppen5@yahoo.com>
2013-12-12 16:01:26 -0800

    tried wss

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -265,7 +265,7 @@ $(function() {
     SP.functions.startWebSocket = function() {
       // ** Agent Presence Stuff ** //
       console.log(".startWebSocket...");
-     var wsaddress = 'ws://' + window.location.host  + "/websocket?clientname=" + SP.username
+     var wsaddress = 'wss://' + window.location.host  + "/websocket?clientname=" + SP.username
 
      var ws = new WebSocket(wsaddress);
       ws.onopen    = function()  { console.log('websocket opened'); };
